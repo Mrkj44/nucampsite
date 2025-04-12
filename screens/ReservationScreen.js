9556351b48cdd25926bcc2c7a02826b8c4c9e408
Mark Johnson <markjohn44@gmail.com>
2025-04-12 00:14:04 -0400

    Custom Side Drawer

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -107,31 +107,6 @@ return (
             accessibilityLabel='Tap me to search for available campsites to reserve'
         />
         </View>
-        
-           {/* <View style={styles.modal}>
-                <Text style={styles.modalTitle}>
-                    Search Campsite Reservations
-                </Text>
-                <Text style={styles.modalText}>
-                    Number of Campers: {campers}
-                </Text>
-                <Text style={styles.modalText}>
-                    Hike-In?: {hikeIn ? 'Yes' : 'No'}
-                </Text>
-                <Text style={styles.modalText}>
-                    Date: {date.toLocaleDateString('en-US')}
-                </Text>
-                <Button
-                    onPress={() => {
-                        setShowModal(!showModal);
-                        resetForm();
-                    }}
-                    color='#5637DD'
-                    title='Close'
-                />
-            </View>*/}
-        
-        
         </Animatable.View>
     </ScrollView>
 )
